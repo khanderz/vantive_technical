83e0b7634eeafbff19571e51f1b343043f39e982
Khanh <82117702+khanderz@users.noreply.github.com>
2025-07-19 17:18:01 -0500

    jest set up

---
+++ jest.config.ts
@@ -0,0 +1,10 @@
+import type { Config } from "jest";
+
+const config: Config = {
+  preset: "ts-jest",
+  testEnvironment: "jsdom",
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx"],
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
+};
+
+export default config;
